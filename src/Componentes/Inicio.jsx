7e2e81382b4a041217ad6c0255901d921de98ac7
FrancoFrigerio <70414444+FrancoFrigerio@users.noreply.github.com>
2021-05-27 21:09:33 -0300

    Update Inicio.jsx

--- src/Componentes/Inicio.jsx
+++ src/Componentes/Inicio.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 
 const Inicio = () => {
     return (
-        <div className="container text-center mt-5 texto-primario">
-            <h5>Esto es una simple "app" web... Felicitaciones estas viendo el comienzo....</h5>
-            <h5>La idea es que sea tipo... una app de recordatorios, o algo asi ya iremo viendo</h5>
+        
+           
             
 
 
             <div className="mt-5">
-                <p className="texto-primario">Para ver la magia tienes que loguearte =)</p>
+                <p className="texto-primario container text-center mt-5">Tienes que loguearte =)</p>
             </div>
-        </div>
+        
     )
 }
 
